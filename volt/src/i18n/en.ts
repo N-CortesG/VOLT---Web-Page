c16186a200872d94c46b5b9d399582210735ff89
Nicolas Cortes Gomez <nicolas.cortesg@opitech.com.co>
2025-05-12 20:32:54 +0200

    feat: Implement internationalization support with Spanish and English translations; update components and layouts to utilize translations

---
+++ volt/src/i18n/en.ts
@@ -0,0 +1,24 @@
+const en = {
+    header: {
+      about: "About Us",
+      services: "Services",
+      contact: "Contact",
+      whatsapp: "Whatsapp"
+    },
+    footer: {
+      linksTitle: "Links",
+      contactTitle: "Contact",
+      followUs: "Follow Us",
+      rights: "All rights reserved.",
+      location: "Cali, Colombia",
+      newsletter: "Join our networks and stay updated with the latest news."
+    },
+    comingSoon: {
+      title: "This page is not available",
+      message: "We are working to improve this section. It will be available soon.",
+      buttonText: "Back to home"
+    }
+  };
+  
+  export default en;
+  
---
+++ volt/src/i18n/es.ts
@@ -0,0 +1,24 @@
+const es = {
+    header: {
+      about: "Sobre Nosotros",
+      services: "Servicios",
+      contact: "Contacto",
+      whatsapp: "Whatsapp"
+    },
+    footer: {
+      linksTitle: "Enlaces",
+      contactTitle: "Contacto",
+      followUs: "Síguenos",
+      rights: "Todos los derechos reservados.",
+      location: "Cali, Colombia",
+      newsletter: "Únete a nuestras redes y entérate de las últimas novedades."
+    },
+    comingSoon: {
+      title: "Esta página no está disponible",
+      message: "Estamos trabajando para mejorar esta sección. Muy pronto estará disponible para ti.",
+      buttonText: "Volver al inicio"
+    }
+  };
+  
+  export default es;
+  
