516ec3ef7689de8883bdcce1de911b29669db50b
Nicolas Cortes Gomez <nicolas.cortesg@opitech.com.co>
2025-05-12 19:30:33 +0200

    feat: Refactor footer and header components to improve accessibility and maintainability
    - Updated Footer.astro to include social links from a new data file.
    - Added aria-labels and roles for better accessibility.
    - Changed contact links to use dynamic values from socialLinks.
    - Refactored Header.astro to include a mobile menu and overlay for improved navigation.
    - Created MobileMenu.astro and Overlay.astro components for better code organization.
    - Introduced socialLinks.ts data file to centralize social media links.

---
+++ volt/src/data/socialLinks.ts
@@ -0,0 +1,13 @@
+// src/data/socialLinks.ts
+
+const socialLinks = {
+    facebook: 'https://facebook.com/voltsolarenergy',
+    instagram: 'https://www.instagram.com/voltsolarenergy_col/',
+    linkedin: 'https://linkedin.com/company/voltsolarenergy',
+    whatsapp: 'https://wa.me/573156619502',
+    email: 'mailto:voltsolarcol@gmail.com',
+    phone: '+57 315 661 9502',
+  };
+  
+  export default socialLinks;
+  
